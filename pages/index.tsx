e8cfc4dab8ff8127ceee9f5804bd8b7d79809a0f
DMCorner <94550721+DMCorner@users.noreply.github.com>
2022-09-29 01:51:42 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -68,7 +68,7 @@ function Home() {
         </div>
 
         <div className='form-inner bg-slate-500 rounded-3xl p-5'>
-              <OutputBox/>
+              <OutputBox  Login={Login} error={error}/>
         </div>
 
           <div></div>
